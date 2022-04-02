bbd5088ac54f0cdfb8dcb1180dffb6b24674a6c5
vicentecrespop <vicentecrespo45@gmail.com>
2022-04-02 11:50:57 -0300

    Atualizei app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello():
-    return "Hello, World!"
+    # POST
+    if request.method == "POST":
+        return "Hello, World!"
+
+
+    #GET
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # POST
+
+
+    # GET
+    return render_template("register.html")
