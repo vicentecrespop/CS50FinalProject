7b646bbdcc2f10928117b6ad2c82fa1a6e07c3be
vicentecrespop <vicentecrespo45@gmail.com>
2022-04-02 14:37:41 -0300

    Coloquei login no app.py e atualizei tudo

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
+from flask import Session
 from helpers import login_required, usd
 from werkzeug.security import check_password_hash, generate_password_hash
 
-# Initialize database
-database = sqlite3.connect("finance.db")
-db = database.cursor()
-
-
+# Configure Application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Log user in
+
+    # Forget any user_id
+    session.clear()
+
+    # Initialize database
+    database = sqlite3.connect("users.db")
+    db = database.cursor()  
+
+    # POST
+    if request.method == "POST":
+        name = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check if username was submitted
+        if not name:
+            return 
+            # Não preencheu nome de usuario!! -------------------------------------------------------
+        
+        # Check if password was submitted
+        if not password:
+            return
+            # Não preencheu a senha!! ----------------------------------------------------------------
+
+        # Get user info from database
+        user_info = db.execute("""SELECT * FROM users WHERE username = ?""", name)
+
+        # Check for valid username and password
+        if len(user_info) != 1 or not check_password_hash(user_info[0]["hash"], password):
+            return 
+            # Nome de usuario ou senha invalidos!! ----------------------------------------------------
+        
+        # Remember which user has logged in
+        session["user_id"] = user_info[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # GET
+    return render_template("login.html")
+
 
 @app.route('/', methods=["GET", "POST"])
 def hello():
@@ -16,12 +62,15 @@ def hello():
     if request.method == "POST":
         return "Hello, World!"
 
-
     #GET
     return render_template("index.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # Initialize database
+    database = sqlite3.connect("users.db")
+    db = database.cursor()     
+
     # POST
     if request.method == "POST":
         # Get username and passwords from user
@@ -29,25 +78,29 @@ def register():
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
-        # Check for valid username
+        # Check for valid username               
         names_registered = db.execute("SELECT username FROM users")       
         for users in names_registered:
             if not username:
-                return 
+                return redirect("#")
                 # Não foi inserido um username ERRO!! --------------------------------------------------------------------
-            if username == users["username"]:
-                return
+            if username == users[0]:
+                return redirect("#")
                 # Nome de usuario ja cadastrado!! ------------------------------------------------------------------------                  
 
         # Check if entered passwords and they match
         if not password or password != confirmation:
-            return
+            return redirect("#")
             # Não preencheu a senha ou senhas diferentes!! ---------------------------------------------------------------
         hash = generate_password_hash(password)
 
         # Register new user into database
-        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
-        return render_template("index.html", username=username, password=password)
+        db.execute("""INSERT INTO users (username, hash) VALUES(?, ?)""", (username, hash))
+
+        # Update and close database
+        database.commit()
+        database.close()
+        return render_template("index.html")
 
 
     # GET
--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 
 
-def login_required(f)
+def login_required(f):
     # Decorate routes to require login
 
     @wraps(f)
