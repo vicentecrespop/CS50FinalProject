f2882019f0ccd51560a3aba11fbc12f212bcb699
vicentecrespop <vicentecrespo45@gmail.com>
2022-04-02 13:52:33 -0300

    Adicionei helper.py

---
+++ helpers.py
@@ -0,0 +1,18 @@
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def login_required(f)
+    # Decorate routes to require login
+
+    @wraps(f)
+
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/logni")
+        return f(*args, **kwargs)
+    return decorated_function
+
+def usd(value):
+    # Format value to USD.
+    return f"${value:,.2f}"
